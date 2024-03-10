01ebc5576a8f21ae247ae91d4db798c1ba5d4850
Tblkva <andreichudnovets@yandex.ru>
2024-03-10 23:38:16 +0300

    Initial commit

---
+++ Characters/__init__.py
@@ -0,0 +1,3 @@
+from FeederAPI.Characters.characters import champs_db
+
+__all__ = (champs_db)
---
+++ Characters/characters.py
@@ -0,0 +1,106 @@
+import requests
+from typing import List, Dict
+from dataclasses import dataclass, field
+import json
+
+@dataclass
+class ChampionStats:
+    hp: int
+    hpperlevel: int
+    mp: int
+    mpperlevel: int
+    movespeed: int
+    armor: int
+    armorperlevel: int
+    spellblock: int
+    spellblockperlevel: int
+    attackrange: int
+    hpregen: int
+    hpregenperlevel: int
+    mpregen: int
+    mpregenperlevel: int
+    crit: int
+    critperlevel: int
+    attackdamage: int
+    attackdamageperlevel: int
+    attackspeedperlevel: int
+    attackspeed: int
+
+
+@dataclass
+class Champion:
+    """По какой то непонятной мне причине id - это имя
+    а key - это цифра - видимо то что я называю айди"""
+    version: str
+    id: str
+    key: str
+    name: str
+    title: str
+    blurb: str
+    info: str
+    image: str
+    tags: str
+    partype: str
+    stats: ChampionStats
+
+    @classmethod
+    def load(cls, data: dict):
+        data['stats'] = ChampionStats(**data['stats'])
+        return cls(**data)
+
+
+@dataclass
+class Champions:
+    by_name: Dict[str, Champion] = field(default_factory=dict)
+    by_id: Dict[int, Champion] = field(default_factory=dict)
+
+    @classmethod
+    def load_from_url(cls):
+        url = 'https://ddragon.leagueoflegends.com/cdn/14.5.1/data/ru_RU/champion.json'
+        result = json.loads(requests.get(url).text)
+        champs: Dict = result['data']
+        by_name = {}
+        by_id = {}
+        for champ_name, champ_data in champs.items():
+            obj = Champion.load(champ_data)
+            by_name.update({obj.name: obj})
+            by_id.update({int(obj.key): obj})
+        return cls(by_name, by_id)
+
+
+champs_db = Champions.load_from_url()
+#
+#
+# @dataclass
+# class ChampionMastery:
+#     puuid: str
+#     championPointsUntilNextLevel: int
+#     chestGranted: bool
+#     championId: int
+#     lastPlayTime: int
+#     championLevel: int
+#     summonerId: str
+#     championPoints: int
+#     championPointsSinceLastLevel: int
+#     tokensEarned: int
+#
+#
+#
+# user_puuid = 'j9eGWo7aDNcrCFSegcc1j0sPTCLiLLxU8z2JWRqoYcRA3Jr0nLXtmi08ySIttpTc1xJrVVdSXv4z7w'
+# user_puuid = '9dUIxu1rGah-WAxCQ-LaDzWLb5lY_gjuI91r96HpLbuFlE06FANaIpeOJAKzt7RBuV4mklek3hlbNA'
+# url = f'https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{user_puuid}?api_key=RGAPI-ca7ff195-03df-4712-b782-66158166f0f0'
+#
+# def main():
+#     result: List[Dict] = json.loads(requests.get(url).text)
+#     obj = [ChampionMastery(**champ) for champ in result]
+#     msg = 'Этот членосос играет на:\n'
+#     for number, champ in enumerate(obj):
+#         champ_obj = champs_db.by_id.get(champ.championId)
+#         msg += f'{champ_obj.name} (ур. {champ.championLevel}) ПТС: {champ.championPoints}\n'
+#         if number == 4:
+#             break
+#     print(msg)
+#
+#
+# if __name__ == '__main__':
+#     main()
